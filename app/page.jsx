44e5b95a8cdeb50bf92eed02c3d5439d0c31f982
Ingram Smith <coovaapp@gmail.com>
2025-08-18 21:45:18 -0400

    fix error on page.jsx for revalidate

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,6 @@
 'use client';
 
-// ✅ Force dynamic rendering and disable ISR for this page
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
-import Hero from '@/components/Hero';
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
