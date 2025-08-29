810f059ce41bfb22894f22a04e637250281a6da7
Ingram Smith <coovaapp@gmail.com>
2025-08-28 20:34:12 -0400

    fix location/gps loads

---
+++ components/MapView.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Fix missing marker icons in Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+export default function MapView() {
+  const [position, setPosition] = useState([38.9072, -77.0369]); // Default Washington DC
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          setPosition([pos.coords.latitude, pos.coords.longitude]);
+        },
+        () => {
+          console.warn("Geolocation not allowed, using default position");
+        }
+      );
+    }
+  }, []);
+
+  return (
+    <div className="w-full h-[400px] rounded-lg overflow-hidden border border-gray-200">
+      <MapContainer
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="h-full w-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>You are here 📍</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
